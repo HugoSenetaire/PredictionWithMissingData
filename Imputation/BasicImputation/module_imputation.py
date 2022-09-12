0e21e0d172a8993e0084be71eb9ca929fe0cc315
HugoSenetaire <hhjs@dtu.dk>
2022-09-12 14:07:32 +0200

    Fix : handle mutliple gpu for large imputation

--- Imputation/BasicImputation/module_imputation.py
+++ Imputation/BasicImputation/module_imputation.py
@@ -52,9 +52,19 @@ class ModuleImputation(Imputation):
       self.module = module
       for param in self.module.parameters():
           param.requires_grad = False
-      
+    
+  
   def imputation_function(self, data, mask, index = None):
       with torch.no_grad():
+        if data.is_cuda and torch.cuda.device_count() > 1:
+          self.module.to("cuda:1")
+          aux_data = data.to("cuda:1")
+          aux_mask = mask.to("cuda:1")
+        if index is not None :
+          aux_index = index.to("cuda:1")
+        else:
+          aux_index = None
         imputation = self.module(data, mask, index = index)
+      imputation = imputation.to(data.device)
       data_imputed = data * mask + (1-mask) * imputation
       return data_imputed
