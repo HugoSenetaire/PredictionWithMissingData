e887d4637ae9d24c5f9a45778ab2588f5f2e1dd0
HugoSenetaire <hhjs@dtu.dk>
2022-09-13 16:05:53 +0200

    fix : forgot to use mean imputation in logistics

--- MixtureOfLogisticsImputation/logistic_mixture_imputation_utils.py
+++ MixtureOfLogisticsImputation/logistic_mixture_imputation_utils.py
@@ -16,6 +16,6 @@ def instantiate_MixtureofLogistics(param):
     mixture = MixtureOfLogistics(input_size=param["input_size"], nb_centers = param["nb_component"], transform_mean=param["transform_mean"], transform_std=param["transform_std"])
     path_weights = os.path.join(param["model_dir"], "mixture_of_logistics.pt")
     mixture.load_state_dict(torch.load(path_weights))
-    mixture_imputation = MixtureOfLogisticsImputation(mixture)
+    mixture_imputation = MixtureOfLogisticsImputation(mixture, mean_imputation = param["mean_imputation"])
     return mixture_imputation
 
