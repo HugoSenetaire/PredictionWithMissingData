94da3b3af8f1a6c62102012d6701669f660ac8f9
HugoSenetaire <hhjs@dtu.dk>
2022-09-13 14:20:17 +0200

    Fix : Dataset to impute do not load everything at once

--- Dataset_imputation/dataset_imputation.py
+++ Dataset_imputation/dataset_imputation.py
@@ -8,18 +8,43 @@ from sklearn.mixture import GaussianMixture
 
 def instantiate_DatasetSamplingImputation(param,):
     """ Instantiate a DatasetSamplingImputation object from the param dictionary. """
-    return DatasetSamplingImputation(data_to_impute = param["data_to_impute"], )
+    return DatasetSamplingImputation(dataset_to_impute = param["dataset_to_impute"], )
 
 
 class DatasetSamplingImputation(nn.Module):
-    def __init__(self, data_to_impute, ):
+    def __init__(self, dataset_to_impute, ):
         super().__init__()
-        self.data_to_impute = data_to_impute
+        self.dataset_to_impute = dataset_to_impute
+        self.loader = None
+        self.iter_loader = None
+        self.batch_size = None
 
+    def create_loader(self, batch_size, num_workers = 4):
+        if batch_size == 1 :
+            self.batch_size = 2
+        self.loader = torch.utils.data.DataLoader(self.dataset_to_impute, batch_size = batch_size, shuffle = True, num_workers = num_workers)
+        self.iter_loader = iter(self.loader)
+        self.batch_size = batch_size
+
+    def next_item(self, batch_size, num_workers = 4):
+        try :
+            current_tensor = next(self.iter_loader)[0]
+        except StopIteration :
+            self.iter_loader = iter(self.loader)
+            current_tensor = next(self.iter_loader)[0]
+        while len(current_tensor)<batch_size:
+            try :
+                current_tensor = torch.cat((current_tensor, next(self.iter_loader)[0]))
+            except StopIteration :
+                self.iter_loader = iter(self.loader)
+                current_tensor = torch.cat((current_tensor, next(self.iter_loader)[0]))
+        return current_tensor[:batch_size]        
 
     def forward(self, x, mask = None, index = None):
         batch_size = x.shape[0]
-        impute_index = np.random.choice(np.arange(len(self.data_to_impute)), batch_size)
-        impute_data = self.data_to_impute[impute_index].to(x.device)
+        if self.loader is None :
+            self.create_loader(batch_size, num_workers = 4)
+        impute_data = self.next_item(batch_size).to(x.device)
+            
         return impute_data
 
